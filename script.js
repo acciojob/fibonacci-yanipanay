d4c5ee3bfe2f868dfb952a1381eafd02e656db9a
yani pranay <33549205+yanipanay@users.noreply.github.com>
2023-07-26 21:31:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
 // your code here
+   let fib[];
+
+	fib[0]=0;
+	fib[1]=1;
+
+	for(let i=2;i<num;i++){
+		fib[i]=fib[i-1]+fib[i-2];
+	}
+
+	return fib[num-1];
 }
 
 module.exports = fibonacci;
